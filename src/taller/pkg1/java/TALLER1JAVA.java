b2b5288321fbaa9612a4ce243a17054196f5fafa
Josemanuel Cantorin Ore <108252539+josemanuel-cantorin-ore@users.noreply.github.com>
2026-08-05 17:56:38 -0500

    Add files via upload

---
+++ src/taller/pkg1/java/TALLER1JAVA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller.pkg1.java;
+
+/**
+ *
+ * @author MC
+ */
+public class TALLER1JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        panel1  f = new panel1();
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+        f.setTitle("Iniciar sesión");
+        
+        
+    }
+    
+    
+}
